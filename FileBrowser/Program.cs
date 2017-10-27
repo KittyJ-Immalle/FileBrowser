682da2bb27df71b5254b491694e06f6601dc1e05
Kitty Jacobs <kitty.jacobs@immalle.eu>
2017-10-27 11:15:10 +0200

    Added lines

--- FileBrowser/Program.cs
+++ FileBrowser/Program.cs
@@ -10,6 +10,38 @@ namespace FileBrowser
     {
         static void Main(string[] args)
         {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Geef een argument mee.");
+            } else
+            {
+                switch(args[0])
+                {
+                    case "-c":
+                        Console.WriteLine("Current directory:");
+                        break;
+                    case "-d":
+                        Console.WriteLine("All subdirectories in current directory:");
+                        break;
+                    case "-f":
+                        Console.WriteLine("All files in current directory:");
+                        break;
+                    case "-h":
+                        Console.WriteLine("----help----");
+                        Console.WriteLine("-c | Shows current directory");
+                        Console.WriteLine("-d | Shows all subdirectories in current directory");
+                        Console.WriteLine("-f | Shows all files in current directory");
+                        Console.WriteLine("-h | Shows help");
+                        break;
+                    default:
+                        Console.WriteLine("Onbekend argument");
+                        break;
+                }
+
+            }
+
         }
+
+        
     }
 }
