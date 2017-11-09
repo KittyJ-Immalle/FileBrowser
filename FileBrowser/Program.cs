c3dac50679cc69bb9a2b3094d0b622248ff2f74d
Kitty Jacobs <kitty.jacobs@immalle.eu>
2017-11-09 21:46:27 +0100

    Added subdirectories + files

--- FileBrowser/Program.cs
+++ FileBrowser/Program.cs
@@ -11,27 +11,34 @@ namespace FileBrowser
     {
         static void Main(string[] args)
         {
+            String[] subdirectories;
+
             if(args.Length == 0)
             {
-                Console.WriteLine("Geef een argument mee.");
+                Console.WriteLine("Please execute with an argument.");
             } else
             {
-                string c = Directory.GetCurrentDirectory();
 
                 switch (args[0])
                 {
                     case "-c":
                         Console.WriteLine("Current directory:");
-
-                        Console.WriteLine(c);
+                        Console.WriteLine(Directory.GetCurrentDirectory());
                         break;
                     case "-d":
                         Console.WriteLine("All subdirectories in current directory:");
-                        
-
+                        subdirectories = Directory.GetDirectories(Directory.GetCurrentDirectory());
+                        foreach (String dir in subdirectories) {
+                            Console.WriteLine(dir);
+                        }
                         break;
                     case "-f":
                         Console.WriteLine("All files in current directory:");
+                        string[] files = Directory.GetFiles(Environment.CurrentDirectory);
+                        foreach (string file in files)
+                        {
+                            Console.WriteLine(Path.GetFileName(file));
+                        }
                         break;
                     case "-h":
                         Console.WriteLine("----help----");
